139812a0be453c3327324dab361fcfe30d0005d1
SunkanmiAyeni <jumokeoayeni@gmail.com>
2024-09-21 13:37:14 +0100

    Tax.py

---
+++ Vehicle.py
@@ -0,0 +1,14 @@
+print("Choose your vehicle")
+a=input("Do you want a car or a bike")
+if a=="car":
+    car=input("Do you want a Toyota or Pagani")
+    if car=="Toyota":
+        print("your toyota car ride is ready")
+    else:
+        print("your pagani car is ready")
+else:
+    bike=input("Honda or Suzuki")
+    if bike=="Honda":
+        print("your honda is ready")
+    else:
+        print("your Suzuki is ready")
---
+++ bill.py
@@ -0,0 +1,7 @@
+unit=int(input("enter the of electric city units you have consumed"))
+if unit< 100:
+    print("Youre ectric city bill is",unit*10)
+elif unit<200:
+    print("your elecric city bill is",unit*20)
+else:
+    print("yor elecric city bill is ",unit*30)
